44a9ab517c572433b000746977dd0236e06aeca2
Qasim Sarfraz <qasim.sarfraz@intechww.com>
2017-12-21 16:30:19 +0000

    Clean build.sh
    With automated builds we don't need this script anymore.

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-docker build -t esailors/aws-ecr-http-proxy:1.13.7-alpine .
-docker tag esailors/aws-ecr-http-proxy:1.13.7-alpine esailors/aws-ecr-http-proxy:latest
-
-docker push esailors/aws-ecr-http-proxy:1.13.7-alpine
-docker push esailors/aws-ecr-http-proxy:latest
