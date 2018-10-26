9d64778ee7c81510368cb04e30078a0bf6f520be
Qasim Sarfraz <qasim.sarfraz@esailors.de>
2018-10-26 08:42:04 +0000

    Retry token

--- files/renew_token.sh
+++ files/renew_token.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
 
-set -e
+set -xe
 
 # update the auth token
 CONFIG=/usr/local/openresty/nginx/conf/nginx.conf
 AUTH=$(grep  X-Forwarded-User $CONFIG | awk '{print $4}'| uniq|tr -d "\n\r")
-TOKEN=$(aws ecr get-login --no-include-email | awk '{print $6}')
+
+# retry till new get new token
+while true; do
+  TOKEN=$(aws ecr get-login --no-include-email | awk '{print $6}')
+  [ ! -z "${TOKEN}" ] && break
+  echo "Warn: Unable to get new token, wait and retry!"
+  sleep 30
+done
+
+
 AUTH_N=$(echo AWS:${TOKEN}  | base64 |tr -d "[:space:]")
 
 sed -i "s|${AUTH%??}|${AUTH_N}|g" $CONFIG
