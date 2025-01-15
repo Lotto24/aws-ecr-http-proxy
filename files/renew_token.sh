063405d1deac92249079232d00329ab1adcc518c
Geoffrey Mersch <geoffrey.mersch@mendix.com>
2025-01-15 18:10:45 +0100

    fix: AWS ECR token too long cause nginx emerg error

--- files/renew_token.sh
+++ files/renew_token.sh
@@ -6,6 +6,7 @@ set -xe
 CONFIG=/usr/local/openresty/nginx/conf/nginx.conf
 AUTH=$(grep  X-Forwarded-User $CONFIG | awk '{print $4}'| uniq|tr -d "\n\r")
 
+set +x
 # retry till new get new token
 while true; do
   TOKEN=$(aws ecr get-login --no-include-email | awk '{print $6}')
@@ -17,6 +18,7 @@ done
 
 AUTH_N=$(echo AWS:${TOKEN}  | base64 |tr -d "[:space:]")
 
-sed -i "s|${AUTH%??}|${AUTH_N}|g" $CONFIG
+echo $AUTH_N > /usr/local/openresty/nginx/token.txt
+set -x
 
 nginx -s reload
