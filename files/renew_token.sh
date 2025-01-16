4afdffc4e7878a7ca2e9775e42ffd52b83423b0b
Geoffrey Mersch <geoffrey.mersch@mendix.com>
2025-01-16 08:49:22 +0100

    fix: replace deprecated aws ecr get-login with get-authorization-token

--- files/renew_token.sh
+++ files/renew_token.sh
@@ -9,16 +9,13 @@ AUTH=$(grep  X-Forwarded-User $CONFIG | awk '{print $4}'| uniq|tr -d "\n\r")
 set +x
 # retry till new get new token
 while true; do
-  TOKEN=$(aws ecr get-login --no-include-email | awk '{print $6}')
+  TOKEN=$(aws ecr get-authorization-token --query 'authorizationData[*].authorizationToken' --output text)
   [ ! -z "${TOKEN}" ] && break
   echo "Warn: Unable to get new token, wait and retry!"
   sleep 30
 done
 
-
-AUTH_N=$(echo AWS:${TOKEN}  | base64 |tr -d "[:space:]")
-
-echo $AUTH_N > /usr/local/openresty/nginx/token.txt
+echo $TOKEN > /usr/local/openresty/nginx/token.txt
 set -x
 
 nginx -s reload
--- files/startup.sh
+++ files/startup.sh
@@ -80,9 +80,9 @@ chmod 600 -R ${AWS_FOLDER}
 set +x
 # add the auth token in default.conf
 AUTH=$(grep  X-Forwarded-User $CONFIG | awk '{print $4}'| uniq|tr -d "\n\r")
-TOKEN=$(aws ecr get-login --no-include-email | awk '{print $6}')
-AUTH_N=$(echo AWS:${TOKEN}  | base64 |tr -d "[:space:]")
-echo $AUTH_N > /usr/local/openresty/nginx/token.txt
+TOKEN=$(aws ecr get-authorization-token --query 'authorizationData[*].authorizationToken' --output text)
+
+echo $TOKEN > /usr/local/openresty/nginx/token.txt
 
 set -x
 # make sure cache directory has correct ownership
